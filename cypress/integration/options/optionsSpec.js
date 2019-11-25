c1ece8e91702395559aa592cd4e9b27afaafbec6
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-11-25 16:47:37 +0000

    Options feature test passing.

--- cypress/integration/options/optionsSpec.js
+++ cypress/integration/options/optionsSpec.js
@@ -1,7 +1,6 @@
 describe("optionsBar", function () {
   it("has a play button", function () {
       cy.visit('localhost:3000')
-      cy.get('.optionsBar').should('visible')
-      cy.get('.play-button').should('visible')
+      cy.get('.options').should('visible')
   })
 })
