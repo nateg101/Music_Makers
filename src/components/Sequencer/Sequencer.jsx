2cec7a88fa8b166e47e6e9f018f70fd4dec9daff
kevindhanna <kevindhanna@gmail.com>
2019-11-25 14:37:02 +0000

    adds Sequencer component

---
+++ src/components/Sequencer/Sequencer.jsx
@@ -0,0 +1,17 @@
+import Nexus from 'nexusui'
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import { Sequencer } from 'react-nexusui'
+
+
+class OurSequencer extends React.Component {
+
+  render() {
+    return (
+     'hello'
+    )
+  }
+
+}
+
+export default OurSequencer
