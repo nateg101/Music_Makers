b6654288d1f5ee479385cdd0c7e8627b28c47ca4
Nabille Helal <nabilleh@outlook.com>
2019-11-26 17:27:10 +0000

    Add map sequencer fields to notes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarMain from './components/navbar/navbar.jsx';
 import OptionsBar from './components/options/optionsBar.jsx';
 import Audio from './components/buttons/audio.jsx'
+import SequencerContainer from './components/SequencerContainer/SequencerComponent/SequencerComponent.jsx'
 
 function App() {
   return (
     <div className="App">
     <NavbarMain/>
-    <Audio/>
+    <SequencerContainer/>
     <div className='options d-flex justify-content-center'>
       <OptionsBar/>
     </div>
--- src/components/SequencerContainer/SequencerComponent/SequencerComponent.jsx
+++ src/components/SequencerContainer/SequencerComponent/SequencerComponent.jsx
@@ -3,6 +3,7 @@ import { Sequencer } from '../../Nexus'
 import { Container, Row, Col, Card } from 'react-bootstrap'
 import ReactDOM from 'react-dom'
 import './SequencerComponent.css'
+import MIDISounds from 'midi-sounds-react';
 
 
 export default class SequencerContainer extends React.Component {
@@ -27,7 +28,35 @@ export default class SequencerContainer extends React.Component {
     console.log(this.myInput.current.offsetWidth)
     this.setState({ width: this.myInput.current.offsetWidth, height: this.myInput.current.offsetWidth });
   };
-  
+
+  playHighB() {
+    this.midiSounds.playChordNow(3, [71], 2.5);
+  }
+
+  playHighA() {
+    this.midiSounds.playChordNow(3, [69], 2.5);
+  }
+
+  playMiddleG() {
+    this.midiSounds.playChordNow(3, [67], 2.5);
+  }
+
+  playMiddleF() {
+    this.midiSounds.playChordNow(3, [65], 2.5);
+  }
+
+  playMiddleE() {
+    this.midiSounds.playChordNow(3, [64], 2.5);
+  }
+
+  playMiddleD() {
+    this.midiSounds.playChordNow(3, [62], 2.5);
+  }
+
+  playMiddleC() {
+    this.midiSounds.playChordNow(3, [60], 2.5);
+  }
+
   render() {
     return (
       <div >
@@ -36,71 +65,78 @@ export default class SequencerContainer extends React.Component {
             <Card className='note-card justify-content-center border-0'>
               B
             </Card>
-            <Sequencer 
-                rows={1} 
+            <Sequencer className='high-b'
+                rows={1}
                 columns={16}
                 size={[this.state.width*0.9412,this.state.height*0.07]}
-                color='#ffff00'/>
+                color='#ffff00' onChange={this.playHighB.bind(this)}/>
+                <div hidden><MIDISounds ref={(ref) => (this.midiSounds = ref)} instruments={[3]} /></div>
           </Row>
           <Row>
             <Card className='note-card justify-content-center border-0'>
               A
             </Card>
-            <Sequencer 
-                rows={1} 
+            <Sequencer
+                rows={1}
                 columns={16}
                 size={[this.state.width*0.9412,this.state.height*0.07]}
-                color='#ee82ee'/>
+                color='#ee82ee' onChange={this.playHighA.bind(this)}/>
+                <div hidden><MIDISounds ref={(ref) => (this.midiSounds = ref)} instruments={[3]} /></div>
           </Row>
           <Row>
             <Card className='note-card justify-content-center border-0'>
               G
             </Card>
-            <Sequencer 
-              rows={1} 
+            <Sequencer
+              rows={1}
               columns={16}
               size={[this.state.width*0.9412,this.state.height*0.07]}
-              color='#ffa500'/>
+              color='#ffa500' onChange={this.playMiddleG.bind(this)}/>
+              <div hidden><MIDISounds ref={(ref) => (this.midiSounds = ref)} instruments={[3]} /></div>
           </Row>
           <Row>
             <Card className='note-card justify-content-center border-0'>
               F
             </Card>
-            <Sequencer 
-              rows={1} 
+            <Sequencer
+              rows={1}
               columns={16}
               size={[this.state.width*0.9412,this.state.height*0.07]}
-              color='#008000'/>
+              color='#008000' onChange={this.playMiddleF.bind(this)}/>
+              <div hidden><MIDISounds ref={(ref) => (this.midiSounds = ref)} instruments={[3]} /></div>
           </Row>
           <Row>
             <Card className='note-card justify-content-center border-0'>
               E
             </Card>
-            <Sequencer 
-                rows={1} 
+            <Sequencer
+                rows={1}
                 columns={16}
                 size={[this.state.width*0.9412,this.state.height*0.07]}
-                color='#0000ff'/>
+                color='#0000ff' onChange={this.playMiddleE.bind(this)}/>
+                <div hidden><MIDISounds ref={(ref) => (this.midiSounds = ref)} instruments={[3]} /></div>
           </Row>
           <Row>
             <Card className='note-card justify-content-center border-0'>
               D
             </Card>
-            <Sequencer 
-                rows={1} 
+            <Sequencer
+                rows={1}
                 columns={16}
                 size={[this.state.width*0.9412,this.state.height*0.07]}
-                color='#4b0082'/>
+                color='#4b0082' onChange={this.playMiddleD.bind(this)}/>
+                <div hidden><MIDISounds ref={(ref) => (this.midiSounds = ref)} instruments={[3]} /></div>
           </Row>
           <Row>
             <Card className='note-card justify-content-center border-0'>
               C
             </Card>
-            <Sequencer 
-                rows={1} 
+            <Sequencer
+                rows={1}
                 columns={16}
                 size={[this.state.width*0.9412,this.state.height*0.07]}
-                color='#ff0000'/>
+                color='#ff0000' onChange={this.playMiddleC.bind(this)}/>
+                <div hidden><MIDISounds ref={(ref) => (this.midiSounds = ref)} instruments={[3]} /></div>
           </Row>
         </Container>
       </div>
