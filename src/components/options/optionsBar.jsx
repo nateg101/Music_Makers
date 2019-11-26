f24adfd67f5836b4053449411840ad942ab9ce70
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-11-26 13:18:22 +0000

    Created optionsBar.css file and moved stylin

--- src/components/options/optionsBar.jsx
+++ src/components/options/optionsBar.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
 import PlayButton from "./buttons/playButton.jsx";
+import './optionsBar.css'
 
 class OptionsBar extends Component {
   render() {
     return (
-      <Card body>
+      <Card className='option-bar' body>
       <PlayButton/>
       </Card>
     )
