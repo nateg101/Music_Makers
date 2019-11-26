4e2b3c6e9371df4e4238ace6c0f969fee2f2acf3
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-11-26 11:49:35 +0000

    Added feature tests to check sequencer renders correctly.

--- cypress/integration/sequencerContainer.test.js
+++ cypress/integration/sequencerContainer.test.js
@@ -2,7 +2,19 @@ describe('SequencerContainer', function() {
   it('renders the sequencer grid', function() {
     cy.visit('/')
     cy.get('.sequencer-container')
-    cy.get('#nexus-ui-sequencer-1')
-    cy.get('.sequencer-container').find('rect').should('have.length', 256)
+    let i = 1
+    for (i; i < 8; ++i) {
+      cy.get(`#nexus-ui-sequencer-${i}`)
+    }
+  })
+
+  it('dsiplays the notes of an octave in the correct order', function() {
+    cy.visit('/')
+    let noteArray = ['B','A','G','F','E','D','C']
+    cy.get('.sequencer-container').first( function() {
+      cy.get('.card').each((Card, index)=> {
+        cy.wrap(Card).should('contain', noteArray[index])
+      })
+    })
   })
 })
