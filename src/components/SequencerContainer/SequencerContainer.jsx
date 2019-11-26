454e8e49515d27965b3de09b6c1d1a2295898c6a
kevindhanna <kevindhanna@gmail.com>
2019-11-26 11:15:31 +0000

    sorted sequencers into rows, added note values

--- src/components/SequencerContainer/SequencerContainer.jsx
+++ src/components/SequencerContainer/SequencerContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Sequencer } from '../Nexus'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Card } from 'react-bootstrap'
 import ReactDOM from 'react-dom'
 import './SequencerContainer.css'
 
@@ -12,6 +12,7 @@ export default class SequencerContainer extends React.Component {
       width: 0,
       height: 0
     }
+    this.myInput = React.createRef()
   }
   componentDidMount() {
     this.updateWindowDimensions();
@@ -23,74 +24,83 @@ export default class SequencerContainer extends React.Component {
   }
 
   updateWindowDimensions = () => {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
+    console.log(this.myInput.current.offsetWidth)
+    this.setState({ width: this.myInput.current.offsetWidth, height: this.myInput.current.offsetWidth });
   };
   
   render() {
     return (
-      <div className='sequencer-container'>
-        <Container>
+      <div >
+        <Container className='sequencer-container' id="notes" ref={this.myInput}>
           <Row>
-            <Col></Col>
-            <Col id="notes">
-              <Row>
-                A
-                <Sequencer 
-                    rows={1} 
-                    columns={16}
-                    size={[this.state.width*0.7,this.state.height*0.07]}
-                    color='#ffff00'/>
-              </Row>
-              <Row>
-                B
-                <Sequencer 
-                    rows={1} 
-                    columns={16}
-                    size={[this.state.width*0.7,this.state.height*0.07]}
-                    color='#ee82ee'/>
-              </Row>
-              <Row>
-                C
-                <Sequencer 
-                  rows={1} 
-                  columns={16}
-                  size={[this.state.width*0.7,this.state.height*0.07]}
-                  color='#ffa500'/>
-              </Row>
-              <Row>
-                D
-                <Sequencer 
-                  rows={1} 
-                  columns={16}
-                  size={[this.state.width*0.7,this.state.height*0.07]}
-                  color='#008000'/>
-              </Row>
-              <Row>
-                E
-                <Sequencer 
-                    rows={1} 
-                    columns={16}
-                    size={[this.state.width*0.7,this.state.height*0.07]}
-                    color='#0000ff'/>
-              </Row>
-              <Row>
-                F
-                <Sequencer 
-                    rows={1} 
-                    columns={16}
-                    size={[this.state.width*0.7,this.state.height*0.07]}
-                    color='#4b0082'/>
-              </Row>
-              <Row>
-                G
-                <Sequencer 
-                    rows={1} 
-                    columns={16}
-                    size={[this.state.width*0.7,this.state.height*0.07]}
-                    color='#ff0000'/>
-              </Row>
-            </Col>
-            <Col></Col>
+            <Card className='note-card justify-content-center border-0'>
+              B
+            </Card>
+            <Sequencer 
+                rows={1} 
+                columns={16}
+                size={[this.state.width*0.9412,this.state.height*0.07]}
+                color='#ffff00'/>
+          </Row>
+          <Row>
+            <Card className='note-card justify-content-center border-0'>
+              A
+            </Card>
+            <Sequencer 
+                rows={1} 
+                columns={16}
+                size={[this.state.width*0.9412,this.state.height*0.07]}
+                color='#ee82ee'/>
+          </Row>
+          <Row>
+            <Card className='note-card justify-content-center border-0'>
+              G
+            </Card>
+            <Sequencer 
+              rows={1} 
+              columns={16}
+              size={[this.state.width*0.9412,this.state.height*0.07]}
+              color='#ffa500'/>
+          </Row>
+          <Row>
+            <Card className='note-card justify-content-center border-0'>
+              F
+            </Card>
+            <Sequencer 
+              rows={1} 
+              columns={16}
+              size={[this.state.width*0.9412,this.state.height*0.07]}
+              color='#008000'/>
+          </Row>
+          <Row>
+            <Card className='note-card justify-content-center border-0'>
+              E
+            </Card>
+            <Sequencer 
+                rows={1} 
+                columns={16}
+                size={[this.state.width*0.9412,this.state.height*0.07]}
+                color='#0000ff'/>
+          </Row>
+          <Row>
+            <Card className='note-card justify-content-center border-0'>
+              D
+            </Card>
+            <Sequencer 
+                rows={1} 
+                columns={16}
+                size={[this.state.width*0.9412,this.state.height*0.07]}
+                color='#4b0082'/>
+          </Row>
+          <Row>
+            <Card className='note-card justify-content-center border-0'>
+              C
+            </Card>
+            <Sequencer 
+                rows={1} 
+                columns={16}
+                size={[this.state.width*0.9412,this.state.height*0.07]}
+                color='#ff0000'/>
           </Row>
         </Container>
       </div>
