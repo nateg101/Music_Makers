b0f752b8a46ae68debd8901be0fbedbb3b2cbaf4
kevindhanna <kevindhanna@gmail.com>
2019-12-02 14:18:41 +0000

    add feature test for collapsing sequencers

--- cypress/integration/sequencers/percussionSequencerSpec.js
+++
@@ -1,7 +0,0 @@
-describe('percussionSequencer', function() {
-  it('renders the drum sequencer components', function() {
-    cy.visit('/')
-    cy.get('.drum-sequencer-wrapper')
-    cy.get('.drum-sequencer-wrapper').get(`#nexus-ui-sequencer-1`)
-  })
-})
