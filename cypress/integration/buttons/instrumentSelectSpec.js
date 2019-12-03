465aba0ad729cd9a951e8320828d7ee153a78c57
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-12-03 16:28:21 +0000

    trying to get playSpec button test to pass.
    Co-Authored-By: null <NabilleH@users.noreply.github.com>

--- cypress/integration/buttons/instrumentSelectSpec.js
+++ cypress/integration/buttons/instrumentSelectSpec.js
@@ -1,6 +1,7 @@
 describe("instrumentSelect", function () {
-  it("it is visible in the Sequencer Header", function () {
+  it("is visible in the Sequencer Header", function () {
       cy.visit('/')
+      cy.wait(2000)
       cy.get('.piano-sequencer-wrapper').should('visible')
       cy.get('.instrument-select').should('visible')
       cy.get(".instrument-select").contains('Electric Piano')
