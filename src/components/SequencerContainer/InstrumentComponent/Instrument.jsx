43c418b0502b0791039641329c95928c9409ae55
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-12-04 10:59:19 +0000

    Added first test for instrument component. Test Passes.
    Co-Authored-By: null <kevindhanna@users.noreply.github.com>

---
+++ src/components/SequencerContainer/InstrumentComponent/Instrument.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './instrument.css'
+
+
+export default class Instrument extends React.Component {
+
+
+  render() {
+      return (
+        <div className='instrument-container'></div>
+    )
+  }
+}
