81620e5c0793835770231954e3783cbcd702a648
kevindhanna <kevindhanna@gmail.com>
2019-12-04 10:30:38 +0000

    add waits to cy.visit to temp fix render timing issue

--- cypress/integration/sequencers/sequencerContainerSpec.js
+++ cypress/integration/sequencers/sequencerContainerSpec.js
@@ -2,6 +2,7 @@ describe('SequencerContainer', function() {
   describe('render piano sequencer', function() {
     it('renders the piano sequencer', function() {
       cy.visit('/')
+      cy.wait(4000)
       cy.get('.sequencer-component')
       let i = 1
       for (i; i < 3; ++i) {
@@ -11,6 +12,7 @@ describe('SequencerContainer', function() {
   
     it('displays the notes of an octave in the correct order', function() {
       cy.visit('/')
+      cy.wait(4000)
       let noteArray = ['B5','A5', 'G5','F5','E5','D5','C5','B4','A4','G4','F4','E4','D4','C4','B3','A3','G3','F3','E3','D3','C3']
       cy.get('.piano-sequencer-wrapper').find('.note-card').each((Card, index)=> {
         cy.wrap(Card).should('contain', noteArray[index])
@@ -29,7 +31,7 @@ describe('SequencerContainer', function() {
 
   it('renders the drum sequencer components', function() {
     cy.visit('/')
-    cty.wait(4000)
+    cy.wait(4000)
     cy.get('.drum-sequencer-wrapper')
     cy.get('.drum-sequencer-wrapper').get(`#nexus-ui-sequencer-1`)
   })
