cf7c43d1ad027a354d948a195e2411adcf1b22f6
kevindhanna <kevindhanna@gmail.com>
2019-12-05 17:37:26 +0000

    tested drums component sends midi calls to midistorage

--- src/components/InstrumentContainer/Instruments/Drums.test.js
+++ src/components/InstrumentContainer/Instruments/Drums.test.js
@@ -5,9 +5,13 @@ import { shallow, mount } from 'enzyme'
 describe('instrument component testing', function() {
   let wrapper
   let storedPercussion
+  let midiStorage = {}
   beforeEach(function() {
     storedPercussion = []
+    midiStorage.MIDIPlugin = {}
+    midiStorage.MIDIPlugin.chordOn = jest.fn()
     wrapper = mount(<Drums
+      midiStorage={midiStorage}
       storedSequencers={storedPercussion}
       drums={[]}
       tempStorage={[]}
@@ -24,5 +28,11 @@ describe('instrument component testing', function() {
   it('renders child components', function(){
     expect(wrapper.find('.drums-container').length).toEqual(1)
     expect(wrapper.find('.sequencer-component')).toBeTruthy()
+  })
+  
+  it('sends calls the midi plugin', function() {
+    wrapper.instance().ready = true
+    wrapper.instance().playDrumNote([1],1,1)
+    expect(midiStorage.MIDIPlugin.chordOn).toHaveBeenCalledWith(1, [48], 100, 0)
   })
 });
