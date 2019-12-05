bef77986480a7986e3faed1e83f0a6b46238f168
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-12-05 14:34:25 +0000

    Refactored lengthy save url test in saveButtonSpec.js

--- cypress/integration/navbar/saveButtonSpec.js
+++ cypress/integration/navbar/saveButtonSpec.js
@@ -13,14 +13,7 @@ describe('SaveButton', function() {
   })
 
   it('saves the url correctly each time', function() {
-    cy.get('rect').first().click()
-    cy.get('rect').first().should('have.css', 'fill', 'rgb(255, 255, 0)')
-
-    cy.get('.save-button').click()
-    cy.get('.Modal-wrapper')
-    cy.get('.url').contains("/?0=IwBj4yunb-GKclq3o5r2e7_gwo4k0s8iyIA~&1=Aw18ZXTt_DFOS1b0c1tQ&2=AxA~")
-
-    cy.contains('X').click()
+    
     cy.visit('http://localhost:3000/?0=IwBj4yunb-GKclq3o5r2e7_gwo4k0s8iyIA~&1=Aw18ZXTt_DFOS1b0c1tQ&2=AxA~')
     cy.get('rect').first().should('have.css', 'fill', 'rgb(255, 255, 0)')
 
