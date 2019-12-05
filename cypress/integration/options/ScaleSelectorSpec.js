4ef8c9628bd03debbc7522603015a118fd232066
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-12-05 16:59:43 +0000

    Added E2E tests for options bar and scale selector

--- cypress/integration/options/ScaleSelectorSpec.js
+++ cypress/integration/options/ScaleSelectorSpec.js
@@ -1,14 +1,22 @@
 describe("scaleSelector", function () {
-  it("has major and minor radio buttons", function () {
+  beforeEach(function() {
     cy.visit('/')
+  })
+  it("has major and minor radio buttons", function () {
     cy.get('.maj-min-buttons').should('visible')
   })
 
   it("has a menu to select key", function () {
-    cy.visit('/')
     cy.get('.key-selector').should('visible')
     cy.get('.key-selector-control').select('C')
     cy.get('.key-selector-control').select('D#')
     cy.get('.key-selector-control').select('B')
+  })
+
+  it("allows you to select key when playing", function () {
+    cy.get('.play-button').click()
+    cy.get('.key-selector-control').select('A')
+    cy.get('.instruments').should('contain', 'F#5')
+    cy.get('.play-button').click()
   })
 })
--- cypress/integration/options/optionsBarSpec.js
+++ cypress/integration/options/optionsBarSpec.js
@@ -1,6 +1,28 @@
 describe("optionsBar", function () {
+  beforeEach(function() {
+    cy.visit('/')
+    cy.get('.option-bar').should('visible')
+  })
   it("has a play button", function () {
-      cy.visit('/')
-      cy.get('.option-bar').should('visible')
+      cy.get('.play-button').should('contain', '▶')
+  })
+
+  it("shows the tempo bar", function() {
+    cy.get('.tempo-slider').should('visible')
+    cy.get('.tempo-slider').should('contain', '120')
+  })
+
+  it("shows the Octaves selector", function() {
+    cy.get('.octaves').should('visible')
+    cy.get('.octaves').should('contain', 'Octaves')
+  })
+
+  it("Shows the key selection form", function() {
+    cy.get('.key-selector-form').should('visible')
+    cy.get('.key-selector-form').should('contain', 'Key - C')
+  })
+
+  it("Shows the key selection radio buttons", function() {
+    cy.get('.maj-min-buttons').should('visible')
   })
 })
