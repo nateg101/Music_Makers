086effa3cf0471224287c2796b5e043802bdbd16
kevindhanna <kevindhanna@gmail.com>
2019-12-05 18:31:21 +0000

    add onReady test for SequencerComponent

--- src/components/InstrumentContainer/Instruments/SequencerComponent/SequencerComponent.test.js
+++ src/components/InstrumentContainer/Instruments/SequencerComponent/SequencerComponent.test.js
@@ -3,8 +3,20 @@ import SequencerComponent from './SequencerComponent'
 import { mount } from 'enzyme'
 
 describe('SequencerComponent Testing', function() {
+  let wrapper
+  let onReady = jest.fn()
+  beforeEach(function() {
+    wrapper = mount(<SequencerComponent 
+                      tempStorage={{}}
+                      onReady={onReady}
+                      scale={[{letter: 'C', value: 1}]}/>)
+  })
   it('renders successfully', function() {
-    let wrapper = mount(<SequencerComponent scale={[{letter: 'C', value: 1}]}/>)
     expect(wrapper.find('.sequencer-component').length).toEqual(2)
+  })
+
+  it('calls the onReady callback when mounded', function(){
+    wrapper.instance().handleOnReady("sequencer")
+    expect(onReady).toHaveBeenCalledWith("sequencer")
   })
 })
