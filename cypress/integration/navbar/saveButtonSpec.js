f3cc7f8ce56b58118cec055bfcaf8b3ff6e2179f
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-12-05 14:44:00 +0000

    Added test to check url can be saved

--- cypress/integration/navbar/saveButtonSpec.js
+++ cypress/integration/navbar/saveButtonSpec.js
@@ -12,8 +12,14 @@ describe('SaveButton', function() {
     cy.contains('Save your song!')
   })
 
+  it('saves the url', function() {
+    cy.get('.save-button').click()
+    cy.get('.Modal-wrapper')
+    cy.get('.url').contains("http://localhost:3000/?0=Aw18ZXTt_DFOS1b0c17Pd_wYUcSaWREA&1=Aw18ZXTt_DFOS1b0c1tQ&2=AxA~&3=Aw18ZXTt_DFOS1b0c17Pd_wYUcSaWREA&4=AxA~")
+  })
+
   it('saves the url correctly each time', function() {
-    
+
     cy.visit('http://localhost:3000/?0=IwBj4yunb-GKclq3o5r2e7_gwo4k0s8iyIA~&1=Aw18ZXTt_DFOS1b0c1tQ&2=AxA~')
     cy.get('rect').first().should('have.css', 'fill', 'rgb(255, 255, 0)')
 
